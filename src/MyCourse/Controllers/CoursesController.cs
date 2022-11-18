7f3c5125a580649a0bc9b70672b18c6aa2d0b98c
Oscar Cambieri <oscar@cambieri.it>
2022-11-18 17:09:39 +0100

    Aggiunte view di contenuto e una view di layout

--- src/MyCourse/Controllers/CoursesController.cs
+++ src/MyCourse/Controllers/CoursesController.cs
@@ -6,12 +6,14 @@ namespace MyCourse.Controllers
     {
         public IActionResult Index()
         {
-            return Content("Sono Index di Courses");
+            // return Content("Sono Index di Courses");
+            return View();
         }
 
         public IActionResult Detail(string id)
         {
-            return Content($"Sono Detail di Courses, ho ricevuto l'id {id}");
+            // return Content($"Sono Detail di Courses, ho ricevuto l'id {id}");
+            return View();
         }
     }
 }
--- src/MyCourse/Controllers/HomeController.cs
+++ src/MyCourse/Controllers/HomeController.cs
@@ -6,7 +6,8 @@ namespace MyCourse.Controllers
     {
         public IActionResult Index()
         {
-            return Content("Sono la Index della Home");
+            // return Content("Sono la Index della Home");
+            return View();
         }
     }
 }
