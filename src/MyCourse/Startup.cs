ae5f36d834fea4da38d587ab628b66d994bcacd2
Oscar Cambieri <oscar@cambieri.it>
2023-02-09 19:21:31 +0100

    Accedere ai valori di configurazione con il servizio IConfiguration

--- src/MyCourse/Startup.cs
+++ src/MyCourse/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyCourse.Models.Services.Application;
 using MyCourse.Models.Services.Infrastructure;
@@ -12,6 +13,13 @@ namespace MyCourse
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -26,9 +34,9 @@ namespace MyCourse
             // services.AddDbContext<MyCourseDbContext>();
             services.AddDbContextPool<MyCourseDbContext>(optionsBuilder =>
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlite("Data Source=Data/MyCourse.db");
-
+                // string connStr = Config.GetSection("ConnectionStrings").GetValue<string>("MyCourse");
+                string connStr = Configuration.GetConnectionString("MyCourse"); // sintassi più comoda
+                optionsBuilder.UseSqlite(connStr);
             });
         }
 
